a939460304948f0569365e383e8823e856f5c636
Shawn Weed <shawn.weed@icloud.com>
2024-08-25 19:16:15 -0400

    Added pages for projects

--- views.py
+++ views.py
@@ -16,13 +16,25 @@ def developer():
         projects = []
         if r.status_code == 200:
             response = r.json()
+            print(r)
             for i in response:
                 projects.append({'name': i['name'], 'desc': i['description'], 'url': i['html_url']})
             return render_template("developer.html", projects=projects)
         else:
-            print('no here')
-            return render_template('developer.html', code = r.status_code)
+            return render_template('developer.html', error = 'Code : ' + str(r.status_code))
             
     except Exception as e:
         error = e
         return render_template('developer.html', error=error)
+    
+@views.route("developer/hardt4il-website")
+def hardt4il():
+    return render_template('hardt4il.html')
+
+@views.route("developer/OptiTrack")
+def optitrack():
+    return render_template('optitrack.html')
+
+@views.route("developer/portfolio-website")
+def porfolio():
+    return render_template('portfolio.html')
